577bd55c8d8cdbb3d8a17a46a6c46b58ac02260f
cjogles <jackson.ogles28@gmail.com>
2020-07-14 09:12:00 -0600

    finished queue tests and data structure questions MD for stacks and queues

--- queue/queue.py
+++ queue/queue.py
@@ -40,29 +40,27 @@ from node import Node
 
 class Queue:
     def __init__(self):
+        self.size = 0
         self.queue = LinkedList()
 
     def __repr__(self):
         return (f"queue: {self.queue}")
 
     def __len__(self):
-        current_node = self.queue.head
-        count = 0
-        if current_node == self.queue.tail or current_node == None:
-            count = 0
-            return count
-        else:
-            while current_node != self.queue.tail:
-                count += 1
-        return count
+        return self.size
 
     def enqueue(self, value):
         self.queue.add_to_tail(value)
+        self.size += 1
 
     def dequeue(self):
-        self.queue.remove_head()
+        if self.size == 0:
+            pass
+        else:
+            self.size -= 1
+            return self.queue.remove_head()
 
-jackson = Queue();
-jackson.enqueue("1")
-jackson.enqueue(21)
-print(jackson.__len__())
+# jackson = Queue();
+# jackson.enqueue("1")
+# jackson.enqueue(21)
+# print(jackson.__len__())
